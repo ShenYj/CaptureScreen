c3d4cb74caf3d59a8096881ab47dbde8c2ab099f
赵凯 <zhaokai@zhaokaidemacbook-pro.local>
2016-10-27 16:22:04 +0800

    Initial Commit

---
+++ CaptureScreen/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CaptureScreen
+//
+//  Created by 赵凯 on 16/10/27.
+//  Copyright © 2016年 赵凯. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
