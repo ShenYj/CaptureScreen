8516e13569cd8539280fd7b1e7a690f45031bc5e
zk1947 <zk1947@163.com>
2016-10-29 09:33:01 +0800

    代码优化

--- CaptureScreen/ViewController.m
+++ CaptureScreen/ViewController.m
@@ -51,7 +51,7 @@
     
     UIImage* image = nil;
     
-    // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了，关键就是第三个参数。
+    // 下面方法，第一个参数表示区域大小。第二个参数表示是否是非透明的。如果需要显示半透明效果，需要传NO，否则传YES。第三个参数就是屏幕密度了，调整清晰度。
     UIGraphicsBeginImageContextWithOptions(scroll.contentSize, YES, [UIScreen mainScreen].scale);
     
     CGPoint savedContentOffset = scroll.contentOffset;
